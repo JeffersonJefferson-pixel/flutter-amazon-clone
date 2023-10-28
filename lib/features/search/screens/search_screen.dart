5b664f6ac5b720a280cd0e89a6061bf0f38edb4c
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2023-10-28 23:03:07 +0700

    add product detail screen

--- lib/features/search/screens/search_screen.dart
+++ lib/features/search/screens/search_screen.dart
@@ -1,6 +1,7 @@
 import 'package:amazon_clone/common/widgets/loader.dart';
 import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:amazon_clone/features/home/widgets/address_box.dart';
+import 'package:amazon_clone/features/product_details/screens/product_details_screen.dart';
 import 'package:amazon_clone/features/search/services/search_services.dart';
 import 'package:amazon_clone/features/search/widgets/search_product.dart';
 import 'package:amazon_clone/models/product.dart';
@@ -119,10 +120,20 @@ class _SearchScreenState extends State<SearchScreen> {
                 ),
                 Expanded(
                   child: ListView.builder(
-                      itemCount: products!.length,
-                      itemBuilder: (context, index) {
-                        return SearchProduct(product: products![index]);
-                      }),
+                    itemCount: products!.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(
+                            context,
+                            ProductDetailsScreen.routeName,
+                            arguments: products![index],
+                          );
+                        },
+                        child: SearchProduct(product: products![index]),
+                      );
+                    },
+                  ),
                 ),
               ],
             ),

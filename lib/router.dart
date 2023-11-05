b1666a75da18df0a1b6f313e143cc2578a6b3464
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2023-11-05 11:48:11 +0700

    add address screen

--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,5 @@
 import 'package:amazon_clone/common/widgets/bottom_bar.dart';
+import 'package:amazon_clone/features/address/screens/address_screen.dart';
 import 'package:amazon_clone/features/admin/screens/add_product_screen.dart';
 import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
 import 'package:amazon_clone/features/home/screens/category_deals_screen.dart';
@@ -54,6 +55,9 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
           product: product,
         ),
       );
+    case AddressScreen.routeName:
+      return MaterialPageRoute(
+          settings: routeSettings, builder: (_) => const AddressScreen());
     default:
       return MaterialPageRoute(
         settings: routeSettings,
